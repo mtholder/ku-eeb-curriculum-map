c696d360adc1f5802cdda9fb033cbe68b4b6565c
Mark T. Holder <mtholder@gmail.com>
2023-12-11 15:04:59 -0600

    data separated

---
+++ data.js
@@ -0,0 +1,1040 @@
+////////////////////////////////////////////////////////////////////////
+// Data to be read from upload, at some point...
+var all_events = [
+  {
+    "foci": "stats",
+    "notes": "The application of statistical methods to data from various fields of biological research. Special emphasis is placed on practical computational procedures.",
+    "outcomes": [
+      0,
+      2,
+      0,
+      0
+    ],
+    "timing": "FIRST_FALL",
+    "title": "841 Biometry I",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      1,
+      0,
+      0,
+      0
+    ],
+    "timing": "FIRST_SPRING",
+    "title": "801 Topics in: Core Topics in Current EEB Res",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      0,
+      2,
+      0,
+      0
+    ],
+    "timing": "FIRST_FALL",
+    "title": "805 Scintfc Intgrty Ecol&Evol Biol",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      1,
+      0,
+      1,
+      0
+    ],
+    "title": "985 Advanced Study",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      2,
+      0
+    ],
+    "title": "985 Advanced Study",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      2,
+      2,
+      0
+    ],
+    "timing": "AFTER_CANDIDACY",
+    "title": "999 Doctoral Dissertation",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      3,
+      3,
+      3,
+      0
+    ],
+    "timing": "AFTER_CANDIDACY",
+    "title": "999 Doctoral Dissertation",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      0,
+      0,
+      0,
+      0
+    ],
+    "timing": "FIRST_MONTH",
+    "title": "Prelim. Advising Meeting",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      0,
+      0,
+      1,
+      0
+    ],
+    "timing": "START_THIRD_SEMESTER",
+    "title": "form RAC",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      2,
+      1,
+      0
+    ],
+    "timing": "THIRD_SEMESTER",
+    "title": "Comp. Oral Exam",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      2,
+      2,
+      2
+    ],
+    "timing": "EARLY_FOURTH_SEMESTER",
+    "title": "Dissertation Prop. Writing",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      2,
+      2,
+      2
+    ],
+    "timing": "FOURTH_SEMESTER",
+    "title": "Dissertation Prop. Defense",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      3,
+      3,
+      2,
+      0
+    ],
+    "timing": "EARLY_FIFTH_SEMESTER",
+    "title": "Dissertation Research",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      3,
+      3,
+      2,
+      3
+    ],
+    "timing": "FIFTH_SEMESTER",
+    "title": "Dissertation Writing",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      3,
+      3,
+      3,
+      3
+    ],
+    "timing": "BEFORE_DEFENSE_DATE",
+    "title": "Finish Dissertation",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      3,
+      3,
+      3,
+      3
+    ],
+    "timing": "DEFENSE_DATE",
+    "title": "Defend Dissertation",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "poster at regional mtg",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "poster at national mtg",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "poster at international mtg",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "present talk at regional mtg",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "present talk at national mtg",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "present talk at international mtg",
+    "type": "event"
+  },
+  {
+    "notes": "EEB genetics, Ecology Friday Lunch series, BIMOL, BigMac or other journal club/discussion group.",
+    "outcomes": [
+      2,
+      2,
+      0,
+      2
+    ],
+    "title": "lead discussion in journal club",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "poster at EEB",
+    "type": "event"
+  },
+  {
+    "outcomes": [
+      2,
+      3,
+      2,
+      2
+    ],
+    "title": "present talk to EEB",
+    "type": "event"
+  },
+  {
+    "notes": "The research skills to be fulfilled is chosen by the student and their PAM committee and RAC.",
+    "outcomes": [
+      0,
+      0,
+      2,
+      0
+    ],
+    "timing": "BEFORE_ORALS",
+    "title": "complete Research Skill",
+    "type": "event"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "600 Introd Biochemistry, Lectures",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "602 Plant Ecology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "622 Paleontology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "623 Paleontology Laboratory",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity,ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "630 Conservatn and Wildlife Biolgy",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "640 Biolgy&Evoltn of Fossil Plants",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "648 Systematics and Macroevolution",
+    "type": "class"
+  },
+  {
+    "foci": "ecology,mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "668 Evolutionary Ecology",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "672 Gene Expression",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "680 Genomics",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: African Biodiversity",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity,ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Biodiversity-Ecosystem Functio",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity,ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Biology of Dispersal",
+    "type": "class"
+  },
+  {
+    "foci": "computing",
+    "outcomes": [
+      0,
+      0,
+      2,
+      0
+    ],
+    "title": "701 Topics in: Computer Science for Biology",
+    "type": "class"
+  },
+  {
+    "foci": "ecology,mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Ecol&Evol Host Microbiome Intr",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Ecology of Infectious Diseases",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Field Botany",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Genome Biology",
+    "type": "class"
+  },
+  {
+    "foci": "ecology,stats",
+    "outcomes": [
+      2,
+      2,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Intro Mathematical Epidemiol",
+    "type": "class"
+  },
+  {
+    "foci": "computing,stats",
+    "outcomes": [
+      0,
+      2,
+      2,
+      0
+    ],
+    "title": "701 Topics in: Likelihood Methods in Biology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Lizards",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Measuring Biodiversity",
+    "type": "class"
+  },
+  {
+    "foci": "ecology,mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Microbiomes: Data to Insight",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      2,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Native&Western Views of Nature",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Natural Resource Management",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Neotropical Biogeo&Diversifctn",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Population Biology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Principles of Insect Morpholgy",
+    "type": "class"
+  },
+  {
+    "foci": "computing",
+    "outcomes": [
+      0,
+      2,
+      2,
+      0
+    ],
+    "title": "701 Topics in: Programming in R",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms,stats",
+    "outcomes": [
+      2,
+      2,
+      0,
+      0
+    ],
+    "title": "701 Topics in: Quantitative Genetics",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      2
+    ],
+    "title": "701 Topics in: Rdgs Evlutnry Genetcs&Genomics",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      2
+    ],
+    "title": "701 Topics in: Readings in Ecosystem Science",
+    "type": "class"
+  },
+  {
+    "foci": "computing,stats",
+    "outcomes": [
+      2,
+      2,
+      2,
+      0
+    ],
+    "title": "701 Topics in: Simulating the Covid Pandemic",
+    "type": "class"
+  },
+  {
+    "foci": "computing,stats",
+    "outcomes": [
+      0,
+      2,
+      2,
+      0
+    ],
+    "title": "701 Topics in: Simultn Modlg Biol",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      0,
+      2,
+      0,
+      2
+    ],
+    "title": "701 Topics in: Writing Scientific Papers",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      0,
+      2,
+      0,
+      2
+    ],
+    "title": "720 Scientific Illustration",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      0,
+      2,
+      0,
+      2
+    ],
+    "title": "735 Scientific Communication",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "743 Population Genetics",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "759 Ecosystems Stewardship",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "782 Principles of Biogeography",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      2
+    ],
+    "title": "787 Introduction to Museum Exhibts",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "798 Intro Collctns Mgmt & Utiliztn",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Adv Topics in Paleobiology",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Biogeochemistry",
+    "type": "class"
+  },
+  {
+    "foci": "computing",
+    "outcomes": [
+      0,
+      2,
+      2,
+      0
+    ],
+    "title": "801 Topics in: Data Carpentry",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      2
+    ],
+    "title": "801 Topics in: EEB Genetics",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity,mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Foundations of Speciation",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Kansas Plants and Landscapes",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Macroevolution",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "801 Topics in: Molecular Systematics",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "815 Advanced Molecular Genetics",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "816 Careers in the Biomed Sciences",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity,mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "848 Phylogenetic Methods",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "860 Principles&Practice Chem Biol",
+    "type": "class"
+  },
+  {
+    "foci": "stats",
+    "outcomes": [
+      2,
+      2,
+      0,
+      0
+    ],
+    "title": "943 Multivariate Data Analysis",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "503 Immunology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "509 Biology of Spiders",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "512 General Virology",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "518 Microbial Genetics",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "520 Marine Biology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "524 Mammalian Paleontology",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "527 Primate Evolutn & Fossil Recrd",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "533 Biology of Fungi",
+    "type": "class"
+  },
+  {
+    "foci": "mechanisms",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "545 Evolution of Development",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "546 Mammalian Physiology",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "547 Mammalian Physiology Laboratry",
+    "type": "class"
+  },
+  {
+    "foci": "stats",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "570 Introduction to Biostatistics",
+    "type": "class"
+  },
+  {
+    "foci": "biodiversity",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "582 Principles of Biogeography",
+    "type": "class"
+  },
+  {
+    "foci": "ecology",
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "594 Forest Ecosystems",
+    "type": "class"
+  },
+  {
+    "outcomes": [
+      2,
+      0,
+      0,
+      0
+    ],
+    "title": "595 Human Genetics",
+    "type": "class"
+  }
+];
+
