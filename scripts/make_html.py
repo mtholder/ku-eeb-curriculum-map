2a31a2d75f4bbcf95d75ca3edf758ddb9e0454ad
Mark T. Holder <mtholder@gmail.com>
2024-01-05 10:04:35 -0600

    generating html

--- scripts/make_html.py
+++ scripts/make_html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import os
+import sys
 from chameleon import PageTemplateLoader
+import os
 
 
 
@@ -13,7 +14,23 @@ def regen():
     if not os.path.isdir(templates_dir):
         raise RuntimeError(f"{templates_dir} is not an existing directory")
     templates = PageTemplateLoader(templates_dir)
-
+    grad_template = templates["grad.pt"]
+    vals = [{"degree_tag": "phd",
+             "degree_name": "Ph.D.",
+             "filename": "phd.html",},
+            {"degree_tag": "mat",
+             "degree_name": "Thesis Master's",
+             "filename": "ma_thesis.html"},
+            {"degree_tag": "mant",
+             "degree_name": "Nonthesis Master's",
+             "filename": "ma_nonthesis.html",},
+             ]
+    for d in vals:
+        fn = d["filename"]
+        with open(fn, "w") as outp:
+            sys.stderr.write(f"... writing {fn} ...\n")
+            outp.write(grad_template(**d))
+            outp.write("\n")
 
 if __name__ == '__main__':
     regen()
